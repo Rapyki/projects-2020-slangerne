eeb6b2535620e3838db9ab8a5b672826f864e295
HenrikKyndal <gzh739@alumni.ku.dk>
2020-03-09 11:34:20 +0100

    mappe

---
+++ inauguralproject/Hen_inauguralproject copy.py
@@ -0,0 +1,23 @@
+def square(x):
+    """ square numpy array
+    
+    Args:
+    
+        x (ndarray): input array
+        
+    Returns:
+    
+        y (ndarray): output array
+    
+    """
+    
+    y = x**2
+    return y
+end
+
+
+    def max(m=1, v=10):
+
+    x = m+v 
+
+    return x
