5e8a53f0a54c2d94c75f5b3f58be941cdf6955c1
HenrikKyndal <gzh739@alumni.ku.dk>
2020-03-09 14:32:44 +0100

    test

--- inauguralproject/Hen.py
+++ inauguralproject/Hen.py
@@ -16,22 +16,22 @@ def square(x):
 
 import numpy as np
 
-def max_f(c,l,epsilon=0.3,v=10,tau_0=0.4,tau_1=0.1,kappa=0.4,m=1):
-    return np.log(c)-v*(L^(1+(1/epsilon))/1+(1/epsilon))
+#def max_f(c,l,epsilon=0.3,v=10,tau_0=0.4,tau_1=0.1,kappa=0.4,m=1):
+   # return np.log(c)-v*(L^(1+(1/epsilon))/1+(1/epsilon))
    
-    m=1
-    v=10
-    epsilon=0.3
-    tau_0=0.4
-    tau_1=0.1
-    kappa=0.4
+   # m=1
+   # v=10
+   # epsilon=0.3
+   # tau_0=0.4
+   # tau_1=0.1
+   # kappa=0.4
     #x = m+wl-[tau_0wl+tau_1*np.max(wl-k,0)]
     #c = np.linspace(0.x)
     #l = np.linspace(0,1)
     #w = np.linspace(0.5,1.5)
     
 
-max = max_f
+#max = max_f
 
 
     
