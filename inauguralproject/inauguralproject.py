6a25e3623e64af78c277ced19d2364b13152ee1f
Christoffer Gade <christoffer@Christoffers-MacBook-Pro.local>
2020-03-09 14:29:47 +0100

    TEST

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,15 +1,34 @@
-def square(x):
-    """ square numpy array
+import numpy as np
+import scipy.optimize as opt
+
+
+# Defining utility
+
+def utility(c,v,l,eps):
+    u = np.log(c) - v*(l**(1+1/eps)/(1+1/eps))
+    return u
+
+# Defining constraint
+
+def eq(m,w,l,tau_0,tau_1,kappa):
+    x = m + w*l - (tau_0*w*l + tau_1*np.max(w*l-kappa,0))
+    return x
+
+# Defining utility based on optimal consumption
+
+def choice(l,w,eps,v,tau_0,tau_1,kappa,m):
+    c = eq(m,w,l,tau_0,tau_1,kappa)
+    return -utility(c,v,l,eps)
+
+# Defining function to optimize labour supply
+
+def optimizer(w,eps,v,tau_0,tau_1,kappa,m):
+    res = opt.minimize_scalar(
+    choice, method='bounded',
+    bounds=(0,1), args=(w,eps,v,tau_0,tau_1,kappa,m))
     
-    Args:
+    l_star = res.x
+    c_star = eq(m,w,l_star,tau_0,tau_1,kappa)
+    utility_star = (c_star,l_star,v,eps)
     
-        x (ndarray): input array
-        
-    Returns:
-    
-        y (ndarray): output array
-    
-    """
-    
-    y = x**2
-    return y
+    return l_star,c_star,utility_star
