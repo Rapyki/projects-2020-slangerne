c4a718b5137d5b321f8e7bb6442b7ab92920abbf
Christoffer Gade <christoffer@Christoffers-MacBook-Pro.local>
2020-03-09 12:42:59 +0100

    test

--- inauguralproject/gade.py
+++ inauguralproject/gade.py
@@ -17,7 +17,11 @@ def square(x):
 
 def f(c,l,v=10, epsilon=0.3):
     #Function is given as: 
-    #import numpy as np
+    import numpy as np
     return  np.log(c)-v*(l**(1+1/epsilon))/(1+1/epsilon)
+
+def c(l,w , m=1, tau0=0.4, tau1=0.1,kappa=0,4):
+    x = m + w*l - (tau0*w*l+tau1*(w*l-kappa)
+    return x
     
     
