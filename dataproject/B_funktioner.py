c662a265a71331a7b41d87c9bf3665a64a6d40f4
Bjørn Hørsted Bremholm <bjornbremholm@bjrns-air.home>
2020-04-02 19:33:40 +0200

    år slettet

--- dataproject/B_funktioner.py
+++ dataproject/B_funktioner.py
@@ -16,3 +16,46 @@ def only_keep_municipalities(df):
     
     return df
 
+def quarter_to_year(df):
+    """ keeps only the first quarter observation  
+
+    Args:
+        df (pd.DataFrame): pandas dataframe with the column "year" as a string
+
+    Returns:
+        df (pd.DataFrame): pandas dataframe
+
+    """ 
+    
+    for val in ['Q1']: 
+        
+        I = df.year.str.contains(val)
+        df = df.loc[I == True] # keep only that one
+        df.year = df.year.str[0:4]
+    
+    return df
+
+def initial_rename_sort(df):
+    """ renames 'Område' to 'municipality' and 'tid' to 'year'. Afterwords it sorts after first municipality and next year
+
+    Args:
+        df (pd.DataFrame): pandas dataframe with the columns "year" and "municipality" as strings
+
+    Returns:
+        df (pd.DataFrame): pandas dataframe
+
+    """ 
+    
+    df.rename(columns = {'OMRÅDE':'municipality'}, inplace=True)
+    df.rename(columns = {'TID':'year'}, inplace=True)
+    df.sort_values(['municipality','year'])
+
+    
+    return df
+
+
+
+
+
+
+
