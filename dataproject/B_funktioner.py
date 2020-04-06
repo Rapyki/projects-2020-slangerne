e2574b8985b2e2b1be5056ae88b763ccce359aaa
Bjørn Hørsted Bremholm <bjornbremholm@Bjrns-Air.domain_not_set.invalid>
2020-04-06 14:11:50 +0200

    Funktioner indsat i dataproject.py

--- dataproject/B_funktioner.py
+++ dataproject/B_funktioner.py
@@ -1,6 +1,7 @@
 
 def quarter_to_year(df):
-    """ keeps only the first quarter observation if the dataset is quarterly, also removes the Q notation
+    """ keeps only the first quarter observation if the dataset is quarterly, also removes the Q notation, so only the year
+    appears
     
 
     Args:
@@ -10,25 +11,25 @@ def quarter_to_year(df):
         df (pd.DataFrame): pandas dataframe
 
     """ 
-    df['year'] = df['year'].astype(str)
+    df['year'] = df['year'].astype(str) #Ensures the year variable is a string
     
-    for year in df.year.str.contains('Q'):
-        if year == True:
+    for year in df.year.str.contains('Q'): #checks whether the year variable is quarterly or yearly
+        if year == True: # If it is quarterly the following code keeps only the first quarter 
             U = df.year.str.contains('Q1')
             df = df.loc[U == True] # keep only that one
-            df.year = df.year.str[0:4]
+            df.year = df.year.str[0:4] # deletes 'Q1'
             break
         else:
-            continue
+            continue #If the data is yearly this function does nothing
         
     return df
 
 def initial_rename(df,varname):
-    """ renames 'Område' to 'municipality' and 'tid' to 'year'. Also renames the data variable
+    """ renames 'OMRÅDE' to 'municipality' and 'TID' to 'year'. Also renames the data variable 'INDHOLD' to a chosen name
 
     Args:
         df (pd.DataFrame): pandas dataframe with the columns "year" and "municipality" as strings
-        varname: the wanted name for the given variable
+        varname: the wanted name for the given variable for 'INDHOLD'
 
     Returns:
         df (pd.DataFrame): pandas dataframe
@@ -38,7 +39,6 @@ def initial_rename(df,varname):
     
     df.rename(columns = {'OMRÅDE':'municipality'}, inplace=True)
     df.rename(columns = {'TID':'year'}, inplace=True)
-
     df.rename(columns = {'INDHOLD':varname}, inplace=True)
     
 
@@ -63,30 +63,30 @@ def sort_reset(df):
 
 
 def only_keep_municipalities_and_years(df,forskel,startaar,slutaar):
-    """ delete all non-municipalities
+    """ delete all non-municipalities, and alså Christiansø and Bornholm
 
     Args:
         df (pd.DataFrame): pandas dataframe with the column "municipality" as a string, 
-        forskel (int) as year variable input (Ras is measured in November while fx. population i january)
+        forskel (int) as year variable input (Ras is measured in November while fx. population in january)
         startaar (int) as the wanted starting year of the dataset
         slutaar (int) as the wanted end year of the dataset
-        keeps only the first quarter observation if the dataset is quarterly, also removes the Q notation
+        
 
     Returns:
         df (pd.DataFrame): pandas dataframe
 
     """
-    df = quarter_to_year(df)
+    df = quarter_to_year(df) #applies the above function
     # Delition list
-    val_list = ['Region', 'Province', 'All Denmark','Christiansø','Bornholm']
+    val_list = ['Region', 'Province', 'All Denmark','Christiansø','Bornholm']  #defines rows to remove
     
     # 
     for val in val_list:
         I = df.municipality.str.contains(val)
-        df = df.loc[I == False] # keep everything else
+        df = df.loc[I == False] # keep everything else than the above rows
 
 
-    
+    #the following keeps the sample period
     df['year'] = df['year'].astype(int)
     df['year'] = df['year'] + forskel
     df = df.loc[df['year'] <= slutaar ]
@@ -97,7 +97,7 @@ def only_keep_municipalities_and_years(df,forskel,startaar,slutaar):
     return df
 
 
-def row_chooser(df,varname,rownames, keep=True):
+def row_chooser(df,varname,rownames, keep=True): #We have not gotten this function to work yet
     """ chooses which rows to keep from af given variable
 
     Args:
--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,17 +1,118 @@
-def only_keep_municipalities(df):
-    """ delete all non-municipalities
+
+def quarter_to_year(df):
+    """ keeps only the first quarter observation if the dataset is quarterly, also removes the Q notation, so only the year
+    appears
+    
 
     Args:
-        df (pd.DataFrame): pandas dataframe with the column "municipality" as a string
+        df (pd.DataFrame): pandas dataframe with the column "year" as a string
+
+    Returns:
+        df (pd.DataFrame): pandas dataframe
+
+    """ 
+    df['year'] = df['year'].astype(str) #Ensures the year variable is a string
+    
+    for year in df.year.str.contains('Q'): #checks whether the year variable is quarterly or yearly
+        if year == True: # If it is quarterly the following code keeps only the first quarter 
+            U = df.year.str.contains('Q1')
+            df = df.loc[U == True] # keep only that one
+            df.year = df.year.str[0:4] # deletes 'Q1'
+            break
+        else:
+            continue #If the data is yearly this function does nothing
+        
+    return df
+
+def initial_rename(df,varname):
+    """ renames 'OMRÅDE' to 'municipality' and 'TID' to 'year'. Also renames the data variable 'INDHOLD' to a chosen name
+
+    Args:
+        df (pd.DataFrame): pandas dataframe with the columns "year" and "municipality" as strings
+        varname: the wanted name for the given variable for 'INDHOLD'
 
     Returns:
         df (pd.DataFrame): pandas dataframe
 
     """ 
     
-    for val in ['Region', 'Province', 'All Denmark']:
+    
+    df.rename(columns = {'OMRÅDE':'municipality'}, inplace=True)
+    df.rename(columns = {'TID':'year'}, inplace=True)
+    df.rename(columns = {'INDHOLD':varname}, inplace=True)
+    
+
+    return df
+
+
+def sort_reset(df):
+    """ sorts after first municipality and then year. Also resets index
+
+    Args:
+        df (pd.DataFrame): pandas dataframe with the columns "year" and "municipality" as strings
+
+    Returns:
+        df (pd.DataFrame): pandas dataframe
+
+    """
+    df = df.sort_values(['municipality','year'])
+    df.reset_index(drop=True, inplace=True)
+
+    return df
+
+
+
+def only_keep_municipalities_and_years(df,forskel,startaar,slutaar):
+    """ delete all non-municipalities, and alså Christiansø and Bornholm
+
+    Args:
+        df (pd.DataFrame): pandas dataframe with the column "municipality" as a string, 
+        forskel (int) as year variable input (Ras is measured in November while fx. population in january)
+        startaar (int) as the wanted starting year of the dataset
+        slutaar (int) as the wanted end year of the dataset
         
+
+    Returns:
+        df (pd.DataFrame): pandas dataframe
+
+    """
+    df = quarter_to_year(df) #applies the above function
+    # Delition list
+    val_list = ['Region', 'Province', 'All Denmark','Christiansø','Bornholm']  #defines rows to remove
+    
+    # 
+    for val in val_list:
         I = df.municipality.str.contains(val)
-        df = df.loc[I == False] # keep everything else
+        df = df.loc[I == False] # keep everything else than the above rows
+
+
+    #the following keeps the sample period
+    df['year'] = df['year'].astype(int)
+    df['year'] = df['year'] + forskel
+    df = df.loc[df['year'] <= slutaar ]
+    df = df.loc[df['year'] >= startaar ]
+    df['year'] = df['year'].astype(str)
+
+
+    return df
+
+
+def row_chooser(df,varname,rownames, keep=True): #We have not gotten this function to work yet
+    """ chooses which rows to keep from af given variable
+
+    Args:
+        df (pd.DataFrame): pandas dataframe  
+        varname: The variable to choose rows from
+        rowname: the rows to either keep or drop
+        keep=True: statement of whether to keep or drop the given rows
+
+    Returns:
+        df (pd.DataFrame): pandas dataframe
+
+    """
+    
+    for val in rownames: 
+        I = df.varname.str.contains(val)
+        df = df.loc[I == keep] 
     
     return df
