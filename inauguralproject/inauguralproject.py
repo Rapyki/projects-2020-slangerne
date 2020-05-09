5e93cc479128a9bf2f4196bfc7f716bb01c24e10
Bjørn Hørsted Bremholm <bjornbremholm@bjrns-air.noko.local>
2020-05-09 22:44:50 +0200

    updates and documentation

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,15 +1,14 @@
 import numpy as np
 import scipy.optimize as opt
 
-
-# Defining utility
-
-
-
 class ConsumerProblem:
+    """ The following class solves the consumerproblem. Firstly the parameters of the model are assigned with intial
+    values. Further it relies on the given equations and constraints of the  utility maximization that is the consumer problem. 
+    Lastly it also calculates the tax revenue when considering N individuals in a model economy.
 
-    # Gives the parameters a defult 
+    """
 
+    # Parameters are assigned with the given initial value
     def __init__(self): 
         self.m = 1 #cash-on-hand
         self.v = 10 #disutility of labor
@@ -19,28 +18,38 @@ class ConsumerProblem:
         self.kappa = 0.4 #cut-off for top labor income tax
         self.w = 0.5 #wage rate
 
-    # Defining Utility
-
+    # Defining the utility equation (the first order condition of the utilitty maximization?)
     def utility(self,c,l):
         u = np.log(c) - self.v*(l**(1+1/self.eps)/(1+1/self.eps))
         return u
 
-    # Defining constraint
-
+    # Defining the budget constraint
     def eq(self,l):
         x = self.m + self.w*l - (self.tau_0*self.w*l + self.tau_1*np.fmax(self.w*l-self.kappa,0))
         return x
 
-
+    # Defining the choice of consumption which is equal to the solution of total ressources x.
     def choice(self,l):
         c = self.eq(l)
         return -self.utility(c,l)
 
 
     def optimizer(self):
-        res = opt.minimize_scalar(
-        self.choice, method='bounded',
-        bounds=(0,1))
+        """ solves the consumer problem by optimizing utility.
+
+        Args:
+            self (list): list of parameter values (which can be modified from the notebook) 
+            
+
+        Returns:
+            l_star: optimal labour supply
+            c_star: optimal consumption
+            u_star: utility given the optimal labour and consumption values.
+            result: print of the complete results
+
+        """
+
+        res = opt.minimize_scalar(self.choice, method='bounded', bounds=(0,1))
     
         l_star = res.x
         c_star = self.eq(l_star)
@@ -50,6 +59,17 @@ class ConsumerProblem:
 
     # Tax for the given Consumption and Labour 
     def tax(self,N=10000):
+        """ solves the consumer problem by optimizing utility.
+
+        Args:
+            self (list): list of parameter values (which can be modified from the notebook) 
+            N (int): number of consumers in the simulation
+            
+
+        Returns:
+            tax_rev: The total tax revenue given the N consumers
+
+        """
         np.random.seed(117)
         w_vec = np.random.uniform(0.5, 1.5, size=N)
         l_opt = np.empty(N)
